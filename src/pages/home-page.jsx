c1f7965ac1451f5e1e4427596b9257baab6cc021
Bruno Diaz <bdiazc90@gmail.com>
2025-12-10 22:31:59 -0500

    class 05 primer fetch

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,80 +1,48 @@
+import { useState, useEffect } from "react";
+
 import Card from "../card-contact";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export default function HomePage(props) {
-    function clickConsole(mensaje) {
-        console.log("soy clickConsole, este es mi mensaje: ", mensaje);
-    }
 
-    const personas = [
-        {
-            name: "Bruno",
-            lastName: "Diaz",
-            edad: 35,
-            behavior: null,
-        },
-        {
-            name: "Diego",
-            lastName: null,
-            edad: null,
-            behavior: clickConsole,
-        },
-        {
-            name: "Renato",
-            lastName: "Perez",
-            edad: 15,
-            behavior: null,
-        },
-        {
-            name: "Santiago",
-            lastName: null,
-            edad: null,
-            behavior: null,
-        },
-        {
-            name: "Fernando",
-            lastName: null,
-            edad: null,
-            behavior: clickConsole,
-        },
-        {
-            name: "Isabel",
-            lastName: null,
-            edad: null,
-            behavior: clickConsole,
-        },
-    ]
+    const [loading, setLoading] = useState(false); // Estado para indicar si la carga está en progreso
+    const [contacts, setContacts] = useState([]); // Estado para almacenar los contactos obtenidos de la API
 
-    function agregarPersona() {
-        personas.push({
-            name: "Nuevo",
-            lastName: "Contacto",
-            edad: 20,
-            behavior: clickConsole,
-        })
-        console.log(personas);
-    }
+    useEffect(() => {
+        // Inicio lógica useEffect:
+        async function fetchContacts() {
+            setLoading(true); // Indica que la carga ha comenzado
+            try {
+                const response = await fetch(`${API_URL}`);
+                const data = await response.json();
+                setContacts(data); // Actualiza el estado con los contactos obtenidos
+            } catch (error) {
+                console.error("Error fetching contacts:", error);
+            } finally {
+                setLoading(false); // Indica que la carga ha finalizado
+            }
+        }
+        fetchContacts();
+        // Final lógica useEffect:
+    }, []);
 
     return (
         <main className="pt-12 max-w-md mx-auto p-4">
             <h1 className="text-5xl font-black mb-4">Contact Manager</h1>
             <h2 className="text-3xl font-bold mb-2">Aplicación para gestionar contactos</h2>
 
-            <button onClick={agregarPersona}>Agregar Persona</button>
+            {loading && <p className="text-xl text-white">Cargando contactos...</p>}
 
             {/* Renderizar múltiples Card con map */}
             {/* Cuando utilizas map para renderizar múltiples elementos, es OBLIGATORIO asignar una key a cada componente */}
             {/* --> Para esto, se utiliza el índice del array como key */}
-            {personas.map((persona, index) => (
+            {contacts && contacts.length > 0 && contacts.map((contact, index) => (
                 <Card
                     key={index}
-                    name={persona.name}
-                    lastName={persona.lastName}
-                    edad={persona.edad}
-                    clickBehavior={persona.behavior}
-                    setFavoriteContact={props.setFavoriteContact}
+                    name={contact.fullname}
                 />
             ))}
-
         </main>
     );
 }
